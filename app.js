36b5c25537e87497910d6b6ae4c45bdfa71df335
Chris Underwood <cunderwoodmn@gmail.com>
2017-12-26 21:23:32 -0800

    add a test for whether the array matches the test and give reward for getting the code.

--- app.js
+++ app.js
@@ -8,4 +8,9 @@ window.addEventListener('keyup', (e) => {
   pressed.push(e.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
   console.log(pressed);
+  if(pressed.join('').includes(secretCode)) {
+    console.log("you have found the secret code");
+    cornify_add();
+  }
+
 });
