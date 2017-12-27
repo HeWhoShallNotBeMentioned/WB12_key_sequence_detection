683b5291fa233ab6646fe75eb918b3acafb25689
Chris Underwood <cunderwoodmn@gmail.com>
2017-12-26 21:13:21 -0800

    add code for array and splice

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 console.log("connected");
 
+const pressed = [];
+secretCode = "montylily";
+
 window.addEventListener('keyup', (e) => {
   console.log(e.key);
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  console.log(pressed);
 });
